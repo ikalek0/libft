d1dd9b68976345d29bb59f270f8c485fd94b9013
ikalek0 <130985225+ikalek0@users.noreply.github.com>
2023-04-19 21:10:23 +0200

    Add files via upload

--- ft_memset.c
+++ ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: inde-la- <inde-la-@student.42urduliz.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/17 16:04:32 by inde-la-          #+#    #+#             */
-/*   Updated: 2023/04/19 18:49:44 by inde-la-         ###   ########.fr       */
+/*   Updated: 2023/04/19 21:10:09 by inde-la-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,15 +19,9 @@ void	*ft_memset(void *b, int c, size_t len)
 	i = 0;
 	while (i < len)
 	{	
-		str[i] = c;
+		str[i] == c;
 		i++;
 	}
 	i = 0;
 	return (str);
-}
-
-int	main(void)
-{
-	printf("%s\n", ft_memset("jlhcasjlacsjlas", 8, 5));
-	return (0);
 }
