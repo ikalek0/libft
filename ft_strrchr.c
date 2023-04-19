9f7fce2de1989140ac2fe2d0649a47ad84a6b1c3
ikalek0 <130985225+ikalek0@users.noreply.github.com>
2023-04-19 19:35:29 +0200

    Add files via upload

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -1,25 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: inde-la- <inde-la-@student.42urduliz.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/04/19 18:58:38 by inde-la-          #+#    #+#             */
-/*   Updated: 2023/04/19 19:35:09 by inde-la-         ###   ########.fr       */
+/*   Created: 2023/04/19 19:19:45 by inde-la-          #+#    #+#             */
+/*   Updated: 2023/04/19 19:33:32 by inde-la-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-#define LIBFT_H
-#include <ctype.h>
-#include <string.h>
+#include "libft.h"
 
-int		ft_isalpha(int c);
-int		ft_isdigit(int c);
-int		ft_isalnum(int c);
-int		ft_isascii(int c);
-int		ft_isprint(int c);
-size_t	ft_strlen(const char *s);
 char	*ft_strrchr(const char *s, int c)
-#endif
+{
+	size_t	len;
+	
+	len = 1 + ft_strlen(s);
+	while (i >= 0)
+	{	
+		if (s[i] = c)
+			return (s[i]);
+		i--;
+	}
+	return (0);
+}
